db2e7b1614a1d1bff5008d4d977f84b6ddc6772e
vivian <vivian.xmt@gmail.com>
2015-03-17 15:58:36 +0800

    12. 打印1-N位最大数 / 转化为递归求数字排序问题
    三种解法
    1.直接打印可能溢出
    2.使用int数组模拟运算
    3.递归求数字排序问题
    
    另外：考虑n为负数及0的情况

---
+++ com/code/interview/Print1ToN_12.java
@@ -0,0 +1,58 @@
+package com.code.interview;
+/**
+ * 输入数字n,按顺序打印出1 ~ n位十进制数。
+ * 
+ * @author Vivian
+ * 
+ */
+public class Print1ToN_12 {
+
+	// 递归求数字排序问题
+	public void print1ToMaxNDigits(int n) {
+
+		if (n <= 0) {
+			return;
+		}
+
+		int[] number = new int[n];
+
+		for (int i = 0; i <= n - 1; i++) {
+			number[i] = 0;
+		}
+		printRecursive(number, 0, n);
+
+	}
+
+	public void printRecursive(int[] number, int cur, int n) {
+		if (cur == n) {
+			print(number);
+		} else {
+			for (int i = 0; i <= 9; i++) {
+				number[cur] = i;
+				printRecursive(number, cur + 1, n);
+			}
+		}
+
+	}
+
+	public void print(int[] number) {
+		boolean start = false;
+
+		for (int i = 0; i <= number.length - 1; i++) {
+			if (!start && number[i] != 0) {
+				start = true;
+			}
+			if (start) {
+				System.out.print(number[i]);
+			}
+		}
+
+		System.out.print("\n");
+	}
+
+	public static void main(String[] args) {
+		Print1ToN_12 test = new Print1ToN_12();
+		test.print1ToMaxNDigits(2);
+	}
+
+}
