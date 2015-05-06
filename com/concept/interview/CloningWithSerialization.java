e642e5b652e8b6653f66c2e7f5789ca5e0b48ee8
vivian <vivian.xmt@gmail.com>
2015-05-06 15:45:45 +0800

    序列化实现Deep Clone

--- com/concept/interview/CloningWithSerialization.java
+++ com/concept/interview/CloningWithSerialization.java
@@ -2,7 +2,11 @@ package com.concept.interview;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OptionalDataException;
+import java.io.Serializable;
 
 /**
  * Cloning with serialization - 序列化
@@ -24,24 +28,52 @@ import java.io.ObjectInputStream;
  */
 public class CloningWithSerialization {
 
-	@SuppressWarnings("unchecked")
-	static T clone(T t) throws Exception {
-		// Check if T is instance of Serializeble other throw
-		// CloneNotSupportedException
-		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+	static class Professor implements Serializable {
+		String name;
+		int age;
 
-		// Serialize it
-		serializeToOutputStream(t, bos);
-		byte[] bytes = bos.toByteArray();
-		ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(
-				bytes));
-
-		// Deserialize it and return the new instance
-		return (T) ois.readObject();
+		Professor(String name, int age) {
+			this.name = name;
+			this.age = age;
+		}
 	}
 
-	public static void main(String[] args) {
+	static class Student implements Serializable {
+		int age;
+		String name;// 常量对象。
+		Professor professor;// 学生1和学生2的引用值都是一样的。
 
+		Student(String name, int age, Professor p) {
+			this.name = name;
+			this.age = age;
+			this.professor = p;
+		}
+
+		public Object deepClone() throws IOException, OptionalDataException,
+				ClassNotFoundException {
+
+			// 将对象写到流里
+			ByteArrayOutputStream bo = new ByteArrayOutputStream();
+			ObjectOutputStream oo = new ObjectOutputStream(bo);
+			oo.writeObject(this);
+
+			// 从流里读出来
+			ByteArrayInputStream bi = new ByteArrayInputStream(bo.toByteArray());
+			ObjectInputStream oi = new ObjectInputStream(bi);
+			return (oi.readObject());
+		}
 	}
 
+	public static void main(String[] args) throws OptionalDataException,
+			ClassNotFoundException, IOException {
+		Professor professor = new Professor("王五", 50);
+		Student student1 = new Student("张三", 18, professor);
+
+		Student student2 = (Student) student1.deepClone();
+		student2.professor.name = "李四";
+		student2.professor.age = 30;
+
+		System.out.println("name=" + student1.professor.name + "," + "age="
+				+ student1.professor.age); // 学生1的教授不改变。
+	}
 }
